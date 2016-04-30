d87fdbbbc5716555cf770316e0b4caedf4180eb6
Szymon "l7ssha" Uglis <szymon.uglis@hotmail.com>
2016-04-30 15:02:54 +0200

    Init

---
+++ lColors-Test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <lColors\lColors.h>
+
+int main() {
+	std::cout << "tak" << std::endl;
+	lColors::SetColor(lColors::Color::LIGHTBLUE);
+	std::cout << "tak" << std::endl;
+
+	int k;
+	std::cin >> k;
+}
